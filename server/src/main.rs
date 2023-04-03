7d83c0c91df5ebd723687b97c2c03f4e2291a6b2
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-04-03 11:30:40 +0200

    A rocket to the moon

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use std::net::Ipv4Addr;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Eatup up and running!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    let config = rocket::Config {
+        address: Ipv4Addr::new(0, 0, 0, 0).into(),
+        ..rocket::Config::debug_default()
+    };
+    rocket::custom(&config).mount("/", routes![hello])
 }
