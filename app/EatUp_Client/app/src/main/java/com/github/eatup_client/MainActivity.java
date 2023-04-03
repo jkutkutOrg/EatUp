6bd57412864fb9e3550b73a3dc940d9bbcad89a7
alexeses <46347629+alexeses@users.noreply.github.com>
2023-04-03 16:50:54 +0200

    feat(MainActivity.java): add fullscreen flag to window on create

--- app/EatUp_Client/app/src/main/java/com/github/eatup_client/MainActivity.java
+++ app/EatUp_Client/app/src/main/java/com/github/eatup_client/MainActivity.java
@@ -3,6 +3,7 @@ package com.github.eatup_client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
