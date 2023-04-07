f426f0eb74a9776e8d813ae6f674d29dc68ba875
alexeses <46347629+alexeses@users.noreply.github.com>
2023-04-07 22:30:19 +0200

    feat(EatUp_Client): add QRActivity and button functionality to MainActivity feat(EatUp_Client): add play-services-vision dependency for QR scanning functionality feat(EatUp_Client): add barcode dependency to AndroidManifest.xml for QR scanning

--- app/EatUp_Client/app/src/main/java/com/github/eatup_client/MainActivity.java
+++ app/EatUp_Client/app/src/main/java/com/github/eatup_client/MainActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnGetStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btnGetStarted = findViewById(R.id.btnGetStarted);
+
+        btnGetStarted.setOnClickListener(v -> {
+            System.out.println("Get Started");
+        });
     }
 }
