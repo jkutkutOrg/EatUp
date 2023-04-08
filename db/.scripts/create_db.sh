98b5e1a8f0e48f87db04f8665b0ccebd7c958bc7
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-04-09 01:58:38 +0200

    db now created with initial load and unistall script created

--- db/.scripts/create_db.sh
+++ db/.scripts/create_db.sh
@@ -12,7 +12,6 @@ docker create \
 	-e POSTGRES_PASSWORD=$DB_USER_PASSWD \
 	-e POSTGRES_USER=$DB_USER \
 	-e POSTGRES_DB=$DB_NAME \
+	-v ./load_db.sql:/docker-entrypoint-initdb.d/load_db.sql \
 	postgres:latest &&
 echo "Done!"
-
-# TODO load data from file
---
+++ db/.scripts/uninstall_all_db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source .secrets ||
+{
+	echo "secrets not created!"
+	exit 1
+}
+
+echo "Removing DB..." &&
+docker stop $DB_CONTAINER_NAME > /dev/null &&
+docker rm $DB_CONTAINER_NAME > /dev/null &&
+echo "Done!"
+
+rm -f .env
+
+echo "Removing DB Web Controller..." &&
+docker stop $WEB_CONTROLLER_CONTAINER_NAME > /dev/null &&
+docker rm $WEB_CONTROLLER_CONTAINER_NAME  > /dev/null &&
+echo "Done!"
