eacf391769ad595a5a9e5e0e4866abaf57328990
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-04-17 08:38:17 +0200

    makefile for windows bug fixed

--- db/.scripts/create_db.sh
+++ db/.scripts/create_db.sh
@@ -5,6 +5,13 @@ source .secrets ||
 	echo "secrets not created!"
 	exit 1
 }
+os=$(uname -s);
+if [ "$os" = "Darwin" ]; then
+    current_dir="$PWD"
+else # Linux
+	current_dir="$(pwd)"
+fi
+
 
 echo "Creating DB..."
 docker create \
@@ -12,6 +19,6 @@ docker create \
 	-e POSTGRES_PASSWORD=$DB_USER_PASSWD \
 	-e POSTGRES_USER=$DB_USER \
 	-e POSTGRES_DB=$DB_NAME \
-	-v ./load_db.sql:/docker-entrypoint-initdb.d/load_db.sql \
+	-v $current_dir/load_db.sql:/docker-entrypoint-initdb.d/load_db.sql \
 	postgres:latest &&
 echo "Done!"
