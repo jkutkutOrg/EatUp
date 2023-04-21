f11ead218cad450f66f7ee607736ce0e3cd2d5ea
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-04-21 13:35:20 +0200

    setup proyect created

---
+++ setup/eatup_setup/src/App.tsx
@@ -0,0 +1,8 @@
+import Message from './Message'
+import './App.css'
+
+function App() {
+  return <div><Message/></div>
+}
+
+export default App
---
+++ setup/eatup_setup/src/Message.tsx
@@ -0,0 +1,5 @@
+function Message() {
+  return <h1>Hello world!</h1>
+}
+
+export default Message;
