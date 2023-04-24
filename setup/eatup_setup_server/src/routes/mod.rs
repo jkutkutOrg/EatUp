f8654235584ac0db0e36e0f250ab1607235d932a
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-04-24 15:31:41 +0200

    refactor of router structure incoming

--- setup/eatup_setup_server/src/routes/mod.rs
+++ setup/eatup_setup_server/src/routes/mod.rs
@@ -10,20 +10,39 @@ pub use router::Router;
 pub use request::Request;
 
 pub fn get_router() -> Router {
-    let mut r = Router::new();
-    r.add_endpoint("/test", test_endpoint)
-        .add_endpoint("/microservices", get_all_microservices);
-    r
+    // let mut r = Router::new();
+    // r.add_endpoint("/test", test_endpoint)
+    //     .add_endpoint("/microservices", get_all_microservices);
+    // r
+    Router::new(
+        vec![
+            // ("/test/3".to_string(), test_endpoint2), // TODO test
+            ("/test".to_string(), test_endpoint1),
+            ("/test/2".to_string(), test_endpoint2),
+            ("/microservices".to_string(), get_all_microservices)
+        ]
+    )
 }
 
 // ----------------- Routes -----------------
 
-fn test_endpoint(
+fn test_endpoint1(
     socket: &Socket,
     _req: Request
 ) {
     let mut cmd = std::process::Command::new("ls");
     cmd.arg("-l");
+    let output = cmd.output().unwrap();
+    let output = String::from_utf8(output.stdout).unwrap();
+    socket.send(Ok(Message::text(output))).unwrap();
+}
+
+fn test_endpoint2(
+    socket: &Socket,
+    _req: Request
+) {
+    let mut cmd = std::process::Command::new("ls");
+    cmd.arg("-a");
     let output = cmd.output().unwrap();
     let output = String::from_utf8(output.stdout).unwrap();
     socket.send(Ok(Message::text(output))).unwrap();
--- setup/eatup_setup_server/src/routes/router.rs
+++ setup/eatup_setup_server/src/routes/router.rs
@@ -15,24 +15,24 @@ enum RouterEndpoint {
 }
 
 impl Router {
-    pub fn new() -> Self {
-        Self {
-            endpoints: HashMap::new()
-        }
-    }
+    // pub fn new() -> Self {
+    //     Self {
+    //         endpoints: HashMap::new()
+    //     }
+    // }
 
-    pub fn add_endpoint(
-        &mut self,
-        endpoint: &str,
-        func: fn (&Socket, Request)
-    ) -> &mut Self {
-        self.recursive_add_endpoint(
-            Request::new(endpoint).unwrap(),
-            func,
-            0
-        );
-        self
-    }
+    // pub fn add_endpoint(
+    //     &mut self,
+    //     endpoint: &str,
+    //     func: fn (&Socket, Request)
+    // ) -> &mut Self {
+    //     self.recursive_add_endpoint(
+    //         Request::new(endpoint).unwrap(),
+    //         func,
+    //         0
+    //     );
+    //     self
+    // }
 
     pub fn handle_request(
         &self,
@@ -49,44 +49,44 @@ impl Router {
         self.recursive_handle_request(socket, req, 0);
     }
 
-    fn recursive_add_endpoint(
-        &mut self,
-        req: Request,
-        func: fn (&Socket, Request),
-        depth: usize
-    ) {
-        match depth.cmp(&(req.endpoint.len() - 1)) {
-            Ordering::Equal => { // Overwrite endpoint
-                self.endpoints.insert(
-                    req.endpoint[depth].clone(),
-                    RouterEndpoint::Endpoint(func)
-                );
-            },
-            Ordering::Less => {
-                let current = req.endpoint[depth].clone();
-                match self.endpoints.get(&current) {
-                    None => {
-                        let mut router = Router::new();
-                        router.recursive_add_endpoint(req, func, depth + 1);
-                        self.endpoints.insert(current, RouterEndpoint::Router(router));
-                    },
-                    Some(endpoint) => {
-                        match endpoint {
-                            RouterEndpoint::Endpoint(_) => {
-                                let mut router = Router::new();
-                                router.recursive_add_endpoint(req, func, depth + 1);
-                                self.endpoints.insert(current, RouterEndpoint::Router(router));
-                            },
-                            RouterEndpoint::Router(r) => {
-                                r.recursive_add_endpoint(req, func, depth + 1);
-                            }
-                        }
-                    }
-                }
-            },
-            Ordering::Greater => panic!("Ups, I don't know what to do")
-        }
-    }
+    // fn recursive_add_endpoint(
+    //     &mut self,
+    //     req: Request,
+    //     func: fn (&Socket, Request),
+    //     depth: usize
+    // ) {
+    //     match depth.cmp(&(req.endpoint.len() - 1)) {
+    //         Ordering::Equal => { // Overwrite endpoint
+    //             self.endpoints.insert(
+    //                 req.endpoint[depth].clone(),
+    //                 RouterEndpoint::Endpoint(func)
+    //             );
+    //         },
+    //         Ordering::Less => {
+    //             let current = req.endpoint[depth].clone();
+    //             match self.endpoints.get(&current) {
+    //                 None => {
+    //                     let mut router = Router::new();
+    //                     router.recursive_add_endpoint(req, func, depth + 1);
+    //                     self.endpoints.insert(current, RouterEndpoint::Router(router));
+    //                 },
+    //                 Some(endpoint) => {
+    //                     match endpoint {
+    //                         RouterEndpoint::Endpoint(_) => {
+    //                             let mut router = Router::new();
+    //                             router.recursive_add_endpoint(req, func, depth + 1);
+    //                             self.endpoints.insert(current, RouterEndpoint::Router(router));
+    //                         },
+    //                         RouterEndpoint::Router(r) => {
+    //                             r.recursive_add_endpoint(req, func, depth + 1);
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         },
+    //         Ordering::Greater => panic!("Ups, I don't know what to do")
+    //     }
+    // }
 
     fn recursive_handle_request(
         &self,
@@ -107,5 +107,90 @@ impl Router {
                 }
             }
         }
+    }
+
+    pub fn new(
+        endpoints: Vec<(String, fn (&Socket, Request))>
+    ) -> Self {
+        let endpoints = endpoints.into_iter().rev()
+            .map(|(endpoint, func)| {
+                let req = Request::new(&endpoint).unwrap();
+                (req, func)
+            })
+            .collect::<Vec<(Request, fn (&Socket, Request))>>();
+        let mut hashmap = &mut HashMap::new();
+        Self {
+            endpoints: Self::fill_recursive(&mut hashmap, endpoints, 0)
+        }
+    }
+
+    fn fill_recursive(
+        map: &mut HashMap<String, RouterEndpoint>,
+        endpoints: Vec<(Request, fn (&Socket, Request))>,
+        depth: usize
+    ) -> HashMap<String, RouterEndpoint> {
+        for (request, function) in endpoints {
+            match depth.cmp(&(request.endpoint.len() - 1)) {
+                Ordering::Equal => {
+                    map.insert(
+                        request.endpoint[depth].clone(), // TODO can be removed?
+                        RouterEndpoint::Endpoint(function)
+                    );
+                },
+                Ordering::Less => {
+                    let current = request.endpoint[depth].clone();
+                    match map.get_mut(&current) {
+                        None => {
+                            let new_router = RouterEndpoint::Router(Router {
+                                endpoints: Router::fill_recursive(
+                                    map,
+                                    vec![(request, function)],
+                                    depth + 1
+                                )
+                            });
+
+                            map.insert(current, new_router);
+                        },
+                        Some(endpoint) => match endpoint {
+                            RouterEndpoint::Endpoint(e) => {
+                                let old_endpoint = *e;
+                                let old_endpoint_request_entrypoint = request.endpoint
+                                    .clone().into_iter().enumerate()
+                                    .filter(|&(i, _)| i <= depth)
+                                    .map(|(_, s)| s).collect::<Vec<String>>();
+                                let old_endpoint_request = Request::new(
+                                    &old_endpoint_request_entrypoint.join("/")
+                                ).unwrap();
+                                let new_router = RouterEndpoint::Router(Router {
+                                    endpoints: Router::fill_recursive(
+                                        map,
+                                        vec![
+                                            (request, function),
+                                            (old_endpoint_request, old_endpoint)
+                                        ],
+                                        depth + 1
+                                    )
+                                });
+                                map.insert(
+                                    current,
+                                    new_router
+                                );
+                            },
+                            RouterEndpoint::Router(_r) => {
+                                // r.endpoints = Router::fill_recursive(
+                                //     map,
+                                //     vec![(request, function)],
+                                //     depth + 1
+                                // );
+                                panic!("Already created router.");
+                            }
+                        }
+                    }
+                },
+                Ordering::Greater => panic!("Depth is greater than endpoint length at endpoint: {}", request.endpoint.join("/"))
+            }
+        }
+        let hashmap: HashMap<String, RouterEndpoint> = map.clone();
+        hashmap
     }
 }
