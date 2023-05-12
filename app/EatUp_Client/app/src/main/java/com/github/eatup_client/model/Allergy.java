37f22567d59af0bb2f767ccf7f17c2105c893340
alexeses <46347629+alexeses@users.noreply.github.com>
2023-05-12 23:27:44 +0200

    refactor(model): add getters and setters to Allergy and Product classes

--- app/EatUp_Client/app/src/main/java/com/github/eatup_client/model/Allergy.java
+++ app/EatUp_Client/app/src/main/java/com/github/eatup_client/model/Allergy.java
@@ -1,2 +1,31 @@
-package com.github.eatup_client.model;public class Allergy {
+package com.github.eatup_client.model;
+
+public class Allergy {
+    private String id;
+    private String name;
+    private String img_id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg_id() {
+        return img_id;
+    }
+
+    public void setImg_id(String img_id) {
+        this.img_id = img_id;
+    }
 }
--- app/EatUp_Client/app/src/main/java/com/github/eatup_client/model/Product.java
+++ app/EatUp_Client/app/src/main/java/com/github/eatup_client/model/Product.java
@@ -1,2 +1,69 @@
-package com.github.eatup_client.model;public class Product {
+package com.github.eatup_client.model;
+
+import java.util.List;
+
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private String img_id;
+    private double price;
+    private List<Allergy> allergies;
+    private List<Category> categories;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImg_id() {
+        return img_id;
+    }
+
+    public void setImg_id(String img_id) {
+        this.img_id = img_id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Allergy> getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(List<Allergy> allergies) {
+        this.allergies = allergies;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
 }
