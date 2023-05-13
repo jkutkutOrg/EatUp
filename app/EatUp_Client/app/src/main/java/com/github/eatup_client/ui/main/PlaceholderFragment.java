33e8669cdb630cceb06e34547d78930debbd30d6
alexeses <46347629+alexeses@users.noreply.github.com>
2023-05-13 18:50:20 +0200

    feat(PlaceholderFragment.java): add RecyclerView and ProductAdapter to display products by category feat(PlaceholderFragment.java): add LiveData and ProductApiService to fetch products by category fix(PlaceholderFragment.java): set category based on fragment index style(fragment_menu.xml): replace ConstraintLayout with FrameLayout and add RecyclerView to display products

--- app/EatUp_Client/app/src/main/java/com/github/eatup_client/ui/main/PlaceholderFragment.java
+++ app/EatUp_Client/app/src/main/java/com/github/eatup_client/ui/main/PlaceholderFragment.java
@@ -1,23 +1,25 @@
 package com.github.eatup_client.ui.main;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.github.eatup_client.R;
+import com.github.eatup_client.api.ProductApiService;
 import com.github.eatup_client.databinding.FragmentMenuBinding;
-import com.github.eatup_client.fragments.AppetizersFragment;
-import com.github.eatup_client.fragments.DessertsFragment;
-import com.github.eatup_client.fragments.DrinksFragment;
-import com.github.eatup_client.fragments.MainCoursesFragment;
-import com.github.eatup_client.fragments.StartersFragment;
+import com.github.eatup_client.model.Product;
+import com.github.eatup_client.utils.ProductAdapter;
+
+import java.util.List;
 
 public class PlaceholderFragment extends Fragment {
 
@@ -26,24 +28,20 @@ public class PlaceholderFragment extends Fragment {
     private PageViewModel pageViewModel;
     private FragmentMenuBinding binding;
 
+    private RecyclerView recyclerView;
+    private ProductAdapter adapter;
+    private LiveData<List<Product>> productListLiveData;
+    private ProductApiService productApiService;
+
+    private String category;
+
     public static Fragment newInstance(int fragmentIndex) {
-
-        switch (fragmentIndex) {
-            case 1:
-                return new StartersFragment();
-            case 2:
-                return new AppetizersFragment();
-            case 3:
-                return new MainCoursesFragment();
-            case 4:
-                return new DessertsFragment();
-            case 5:
-                return new DrinksFragment();
-            default:
-                throw new IllegalArgumentException("Invalid fragment index");
-        }
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, fragmentIndex);
+        fragment.setArguments(bundle);
+        return fragment;
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +52,27 @@ public class PlaceholderFragment extends Fragment {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
         pageViewModel.setIndex(index);
+
+        // set the category based on the index
+        switch (index) {
+            case 1:
+                category = "Starters";
+                break;
+            case 2:
+                category = "Appetizers";
+                break;
+            case 3:
+                category = "Main Courses";
+                break;
+            case 4:
+                category = "Desserts";
+                break;
+            case 5:
+                category = "Drinks";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid fragment index " + index);
+        }
     }
 
     @Override
@@ -64,13 +83,19 @@ public class PlaceholderFragment extends Fragment {
         binding = FragmentMenuBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.sectionLabel;
-        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+        recyclerView = binding.rvProducts;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        adapter = new ProductAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+
+        productApiService = new ProductApiService(getContext());
+        productListLiveData = productApiService.getProductsByCategory(category);
+        productListLiveData.observe(getViewLifecycleOwner(), productList -> {
+            adapter.setProducts(productList);
+            Log.i(category, "Products: " + productList.get(0).getName());
         });
+
         return root;
     }
 
