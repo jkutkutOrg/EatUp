44e9b28ac71ba5bc4e3e71adb737d9b34f639c65
Jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-06-07 09:24:36 +0200

    staff: landing page updated

--- staff_web/eatup_staff_web/src/pages/Landing.tsx
+++ staff_web/eatup_staff_web/src/pages/Landing.tsx
@@ -6,24 +6,17 @@ interface Props {
 }
 
 const Landing = ({onBegin}: Props) => {
-    return <div className='container-fluid eatup-landing  text-center'>
-        <div className='row'>
-            <div className='col-8'>
-                <h2 className='landing-header'>Satisfy your cravings with a tap!</h2>
-            </div>
-            <div className='col-3'>
-                <img className="landing-logo" src={logo} alt="logo" />
-            </div>
-        </div>
-        <br />
-        <br />
+    return <div className='container-fluid eatup-landing text-center'>
         <div className='row'>
             <div className='col'>
-                <img className="landing-img" src={landingImg} alt="landing image" />
+                <h2 className='landing-header'>Satisfy your cravings with a tap!</h2>
             </div>
         </div>
-        <br />
-        <br />
+        <div className='row'>
+            <div className='col'>
+                <img className="landing-img img-fluid" src={landingImg} alt="landing image" />
+            </div>
+        </div>
         <div className='row'>
             <div className='col'>
                 <button className='btn btn-primary btn-lg landing-btn' onClick={onBegin}>Begin</button>
