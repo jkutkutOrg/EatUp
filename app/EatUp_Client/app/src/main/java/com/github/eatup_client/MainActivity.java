7bba2f9ff6d7cd8ede5743c6e3e426229acb638f
alexeses <46347629+alexeses@users.noreply.github.com>
2023-06-08 21:45:23 +0200

    feat(android): add network availability check and dialog to MainActivity

--- app/EatUp_Client/app/src/main/java/com/github/eatup_client/MainActivity.java
+++ app/EatUp_Client/app/src/main/java/com/github/eatup_client/MainActivity.java
@@ -1,11 +1,15 @@
 package com.github.eatup_client;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.eatup_client.api.ProductApiService;
@@ -28,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         productApiService = new ProductApiService(this);
+
+        if (!isNetworkAvailable()) {
+            showNoInternetDialog();
+        }
 
         productApiService.loadSessions();
 
@@ -55,5 +63,37 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
+    }
+
+    /**
+     * Checks if the device is connected to the internet.
+     */
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isConnected();
+        }
+        return false;
+    }
+
+    /**
+     * Shows a dialog if the device is not connected to the internet.
+     */
+    private void showNoInternetDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("No Internet Connection");
+        builder.setMessage("Please check your internet connection and try again");
+        builder.setPositiveButton("Retry", (dialog, which) -> {
+            if (isNetworkAvailable()) {
+                setContentView(R.layout.activity_main);
+            } else {
+                showNoInternetDialog();
+            }
+        });
+        builder.setNegativeButton("Exit", (dialog, which) -> finish());
+        builder.setCancelable(false);
+        builder.show();
     }
 }
